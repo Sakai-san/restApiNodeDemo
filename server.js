8adcadcd67d7c1e581606d7bb2e0d89445fe0eba
Sakai-san <o_edo@hotmail.com>
2018-12-07 09:32:30 +0100

    Reduce amont of imported libraries.

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const express = require("express");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 const faker = require("faker");
-const lodash = require("lodash");
+const times = require("lodash.times");
+const random = require("lodash.times");
 const db = require("./models");
 const apiPost = require("./app/api/post");
 const apiAuthor = require("./app/api/author");
 
 const app = express();
-app.use(bodyParser.json());                                                                                                
+app.use(bodyParser.json());
 app.use(express.static("app/public"));
 
 apiPost(app, db);
 apiAuthor(app, db);
 
-db.sequelize.sync().then( () => {
+db.sequelize.sync().then(() => {
   // populate author table with dummy data
   db.author.bulkCreate(
-    lodash.times(10, () => ({
+    times(10, () => ({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName()
     }))
   );
   // populate post table with dummy data
   db.post.bulkCreate(
-    lodash.times(10, () => ({
+    times(10, () => ({
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
-      authorId: lodash.random(1, 10)
+      authorId: random(1, 10)
     }))
   );
-  app.listen(8080, () => 
-    console.log("App listening on port 8080!")
-  );
+  app.listen(8080, () => console.log("App listening on port 8080!"));
 });
