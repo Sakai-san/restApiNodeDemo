6381ebaf3d04d80aafc1116847bbd1066faa387f
Sakai-san <o_edo@hotmail.com>
2019-07-08 18:31:46 +0200

    Replace deprecated sequelize findById.

--- app/api/author.js
+++ app/api/author.js
@@ -1,5 +1,5 @@
 module.exports = (app, db) => {
-  app.get( "/author/:id", (req, res) =>
-    db.author.findById(req.params.id).then( (result) => res.json(result))
+  app.get("/author/:id", (req, res) =>
+    db.author.findByPk(req.params.id).then(result => res.json(result))
   );
-}
+};
